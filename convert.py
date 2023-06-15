c15ce8768be13976d6bef88e8816e4689f1133a8
Calvadoss <124347873+Nhannguyenus24@users.noreply.github.com>
2023-06-15 18:08:26 +0700

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,21 @@
+def convert(number, base):
+    num = 1
+    result = ""
+    list = ["A", "B", "C", "D", "E", "F"]
+    while num <= number:
+        num *= base
+    num //= base
+    while num != 0:
+        remainder = number % num
+        number -= remainder
+        res = number // num
+        if res >= 10:
+            result += list[res - 10]
+        else:
+            result += str(res)
+        number = remainder
+        num //= base
+    return result
+
+str = convert(31,16)
+print(str)
